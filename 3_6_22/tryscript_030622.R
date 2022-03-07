c0f6ed07f738cb33170f3b671c1d61a71ca6b71e
rickzhouwen <rickzhouwen@gmail.com>
2022-03-06 23:46:20 -0700

    tidytuesday added"

--- 3_6_22/tryscript_030622.R
+++ 3_6_22/tryscript_030622.R
@@ -10,5 +10,15 @@ for (s in 1:S)
 }  
 mean(y<0.2) - pnorm(0.2)
 
-hist(y)
 
+# add data from tidy pacakge, github: tidytuesday 03/06/22
+library("tidyverse")
+
+tuesdata <- tidytuesdayR::tt_load('2022-02-22')
+tuesdata <- tidytuesdayR::tt_load(2022, week = 8)
+
+freedom <- tuesdata$freedom
+
+# Or read in the data manually
+
+freedom <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-02-22/freedom.csv')
